3c80725130df45d81eb4f5fdf00fe4a656e42faf
tomasdelima <tomasdelima@gmail.com>
2014-12-06 13:20:46 -0200

    Second commit

--- www/js/index.js
+++ www/js/index.js
@@ -1,44 +1,15 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-var line = "%0D%0A"
+var line = "\n"
 
 var app = {
-  // Application Constructor
   initialize: function() {
     this.bindEvents()
   },
-  // Bind Event Listeners
-  //
-  // Bind any events that are required on startup. Common events are:
-  // 'load', 'deviceready', 'offline', and 'online'.
   bindEvents: function() {
     document.addEventListener('deviceready', this.onDeviceReady, false)
   },
-  // deviceready Event Handler
-  //
-  // The scope of 'this' is the event. In order to call the 'receivedEvent'
-  // function, we must explicitly call 'app.receivedEvent(...);'
   onDeviceReady: function() {
     app.receivedEvent('deviceready')
   },
-  // Update DOM on a Received Event
   receivedEvent: function(id) {
     var parentElement = document.getElementById(id)
     var listeningElement = parentElement.querySelector('.listening')
@@ -94,29 +65,7 @@ function showResults() {
   }
   $('#total').text( app.totalScore() )
   $('#risk').text( app.risk() )
-  $('.answers').addClass('hidden')
-  // $('.send-email').attr('href', 'mailto:'+app.mailTo+'?subject='+buildEmailSubject()+'&body='+buildEmailBody())
-}
-
-function sendEmail () {
-  window.location.href = 'mailto:'+app.mailTo+'?subject='+buildEmailSubject()+'&body='+buildEmailBody()
-}
-
-function buildEmailBody() {
-  body = 'Nome: ' + app.patientName() + line
-  body += 'Número do prontuário: ' + app.patientRegistry() + line + line
-  body += 'Perguntas:' + line
-  for(i=1; i < 7; i++) {
-    q = 'question' + i
-    a = app.result[q] ? 'Sim' : 'Não'
-    body += '  ' +i + ') ' + app.questions[q] + ": " + a + line
-  }
-  body += line + 'Resultado: ' + app.risk()
-  return body
-}
-
-function buildEmailSubject() {
-  return 'Resultado do teste "Escore de Fragilidade" para ' + app.patientName()
+  $('.answers').addClass('hidden')  
 }
 
 function nextQuestion() {
@@ -136,13 +85,34 @@ function score (n) {
   nextQuestion()
 }
 
-function send(){
-  var args = {
-    subject: 'Hi there',
-    body: 'message to suggest you',
-    toRecipients: 'miemail@gmail.com'
-  };
-  cordova.exec(null, null, "EmailComposer", "showEmailComposer", [args]);
+function buildEmailBody() {
+  body = 'Nome: ' + app.patientName() + line
+  body += 'Número do prontuário: ' + app.patientRegistry() + line + line
+  body += 'Perguntas:' + line
+  for(i=1; i < 7; i++) {
+    q = 'question' + i
+    a = app.result[q] ? 'Sim' : 'Não'
+    body += '  ' +i + ') ' + app.questions[q] + ": " + a + line
+  }
+  body += line + 'Resultado: ' + app.risk()
+  return body
 }
+
+function buildEmailSubject() {
+  return 'Resultado do teste "Escore de Fragilidade" para ' + app.patientName()
+}
+
+function sendEmail () {
+  cordova.plugins.email.open({
+    to:          [app.mailTo], 
+    cc:          [], 
+    bcc:         [], 
+    attachments: [], 
+    subject:     buildEmailSubject(), 
+    body:        buildEmailBody(), 
+    isHtml:      false, 
+  });
+}
+
 
 app.initialize();
